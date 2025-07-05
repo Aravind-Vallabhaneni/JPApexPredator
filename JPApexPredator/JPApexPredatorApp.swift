4ca797abe7730f3967df55dafe6a7b0569148a77
aravind vallabhaneni <89920295+Aravind-Vallabhaneni@users.noreply.github.com>
2025-07-05 19:41:29 +0530

    Initial Commit

---
+++ JPApexPredator/JPApexPredatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorApp.swift
+//  JPApexPredator
+//
+//  Created by Aravind vallabhaneni on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
