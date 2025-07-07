18d095fbea9853b1413bfdac5cf898d7f5c29853
aravind vallabhaneni <aravindvalla07@gmail.com>
2025-07-07 17:11:48 +0530

    Added navigation stack, and added search bar with search functionality.

--- JPApexPredator/ContentView.swift
+++ JPApexPredator/ContentView.swift
@@ -9,35 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
 
+    @State var searchText: String = ""
     var body: some View {
         let predators = Predators()
-
-        List(predators.apexPredators) { predators in
-            
-            HStack{
-                
-                //Image
-                Image(predators.Image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100,height: 100)
-                    .shadow(color: .white, radius: 1)
-                
-                VStack(alignment: .leading) {
-                    
-                    Text(predators.name)
-                        .fontWeight(.bold)
-                    
-                    //type
-                    Text(predators.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predators.type.backgroundColor)
-                        .clipShape(.capsule)
-                }
+        
+        var filteredDinos: [ApexPredator] {
+            if searchText.isEmpty {
+                return predators.apexPredators
+            } else {
+                return predators.apexPredators.filter{ predator in
+                    predator.name.localizedCaseInsensitiveContains(searchText)}
             }
+        }
+        NavigationStack{
+                List(filteredDinos) { predators in
+                    NavigationLink{
+                        Image(predators.Image)
+                            .resizable()
+                            .scaledToFit()
+                        
+                    } label: {
+                        
+                        HStack{
+                            
+                            Image(predators.Image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100,height: 100)
+                                .shadow(color: .white, radius: 1)
+                            
+                            VStack(alignment: .leading) {
+                                
+                                Text(predators.name)
+                                    .fontWeight(.bold)
+                                
+                                Text(predators.type.rawValue.capitalized)
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .padding(.horizontal, 13)
+                                    .padding(.vertical, 5)
+                                    .background(predators.type.backgroundColor)
+                                    .clipShape(.capsule)
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("Apex Predators")
+                .searchable(text: $searchText)
+                .autocorrectionDisabled()
+                .animation(.default, value: searchText)
         }
         .preferredColorScheme(.dark)
     }
